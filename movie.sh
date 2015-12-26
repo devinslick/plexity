c4e387d3d951e292dcc1c098ad5171e71296fb8b
Devin Slick <git@devinslick.com>
2015-12-26 09:21:09 -0600

    Create movie.sh

---
+++ movie.sh
@@ -0,0 +1,13 @@
+find /mnt/files/Complete/ -type f -name '*.jpg' -delete
+find /mnt/files/Complete/ -type f -name '*.jpeg' -delete
+find /mnt/files/Complete/ -type f -name '*.txt' -delete
+find /mnt/files/Complete/ -type f -name '*.srt' -delete
+find /mnt/files/Complete/ -type f -name '*.smi' -delete
+find /mnt/files/Complete/ -type f -name '*.idx' -delete
+find /mnt/files/Complete/ -type f -name '*.sub' -delete
+find /mnt/files/Complete/ -type f -name '*.nfo' -delete
+find /mnt/files/Complete/ -type f -name 'RARBG.COM.mp4' -delete
+find /mnt/files/Complete/ -type f -name 'sample.avi' -delete
+sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/Complete/" -r --format "{n} - {y}" --output /mnt/files/Movies/
+find /mnt/files/Complete/ -empty -type d -delete
+find /mnt/files/Complete/
