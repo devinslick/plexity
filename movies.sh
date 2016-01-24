69c8a74e79ac33e63234451a10b77da914983ef3
Devin Slick <git@devinslick.com>
2016-01-24 11:46:16 -0600

    Update movies.sh

--- movies.sh
+++ movies.sh
@@ -1,42 +1,88 @@
-#!/bin/sh
+#!/bin/bash
 
-#autoupdate
-git >/dev/null 2>/dev/null
-if [ $? -eq 127 ]; then
-  echo "Error: You need to have git installed for this to work"
-  exit 1
+if [ ! -d "/mnt/files/Complete/" ]; then
+  echo There are no completed downloads in /mnt/files/Complete/
+  exit
 fi
-pushd "$(dirname "$0")" >/dev/null
-if [ ! -d .git ]; then
-  echo "Error: This is not a git repository, auto update only works if you've done a git clone"
-  exit 1
-fi
-git status | grep "git commit -a" >/dev/null 2>/dev/null
-if [ $? -eq 0 ]; then
-  echo "Error: You have made changes to the script, cannot auto update"
-  exit 1
-fi
-echo -n "Auto updating..."
-git pull >/dev/null
-if [ $? -ne 0 ]; then
-  echo 'Error: Unable to update git, try running "git pull" manually to see what is wrong'
-  exit 1
-fi
-echo "OK"
-popd >/dev/null
-$0 ${ALLARGS} -U
 
-find /mnt/files/Complete/ -type f -size -50M -exec rm {} +
-mkdir -p /mnt/files/Movies/1080/
-mkdir -p /mnt/files/Movies/720/
-find /mnt/files/Complete/ -iname "*1080*" -exec mv {} /mnt/files/Movies/1080/ \;
-find /mnt/files/Complete/ -iname "*720*" -exec mv {} /mnt/files/Movies/720/ \;
-sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Movies/1080/ -r --format "{n} - {y} - 1080p" --output "/mnt/files/Movies/" -non-strict
-sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Movies/720/ -r --format "{n} - {y} - 720p" --output "/mnt/files/Movies/" -non-strict
+find /mnt/files/Complete/ -type f -size -75M -exec rm {} +
+mkdir -p /mnt/files/Queue/
+mkdir -p /mnt/files/Movies/
+
+echo Checking for 1080p movies...
+find /mnt/files/Complete/ -iname "*1080*" -type f -exec mv {} /mnt/files/Queue/ \; >/dev/null
+sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Queue/ -r --format "{n} - {y} - 1080p" --output "/mnt/files/Movies/" -non-strict
 find /mnt/files/Complete/ -empty -type d -delete
-find /mnt/files/Movies/ -empty -type d -delete
-echo Moving files from /mnt/files/Movies/ to /mnt/share/Movies/...
-ls /mnt/files/Movies/
-echo ...
-mv /mnt/files/Movies/* /mnt/share/Movies/
-echo Complete.
+
+if [ -d "/mnt/files/Complete/" ]; then
+  echo More movies exist, checking for 720p...
+  find /mnt/files/Complete/ -iname "*720*" -type f -exec mv {} /mnt/files/Queue/ \; >/dev/null
+  sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Queue/ -r --format "{n} - {y} - 720p" --output "/mnt/files/Movies/" -non-strict
+  find /mnt/files/Complete/ -empty -type d -delete
+fi
+
+if [ -d "/mnt/files/Complete/" ]; then
+  echo Checking for 480p movies...
+  find /mnt/files/Complete/ -name "*480*" -type f -exec mv {} /mnt/files/Queue/ \; >/dev/null
+  sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Queue/ -r --format "{n} - {y} - 480p" --output "/mnt/files/Movies/" -non-strict
+  find /mnt/files/Complete/ -empty -type d -delete
+fi
+
+if [ -d "/mnt/files/Complete/" ]; then
+  echo Checking for 360p movies...
+  find /mnt/files/Complete/ -name "*360p*" -type f -exec mv {} /mnt/files/Queue/ \; >/dev/null
+  sudo /root/scripts/filebot/filebot.sh -rename /mnt/files/Queue/ -r --format "{n} - {y} - 480p" --output "/mnt/files/Movies/" -non-strict
+  find /mnt/files/Complete/ -empty -type d -delete
+fi
+
+
+
+find /mnt/files/Queue/ -empty -type d -delete
+find /mnt/files/Complete/ -empty -type d -delete
+
+echo Done with searching for resolution-named movies.  Checking for movies without resolution in the filename...
+if [ -d "/mnt/files/Complete/" ]; then
+  for movie in $(find "/mnt/files/Complete/" -iname '*' -type f); do 
+    echo Checking resolution of $movie
+    height=$(ffprobe -v quiet -print_format json -show_format -show_streams "$movie"  | grep height | grep -oE "[[:digit:]]{1,}" | head -n 1)
+    if [ $height -ge 800 ]; then
+      echo 1080p
+      mv $movie "/mnt/files/Queue/"
+      sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/Queue/" -r --format "{n} - {y} - 1080p" --output "/mnt/files/Movies/" -non-strict
+    elif [ $height -ge 530 ]; then
+      echo 720p
+      mv $movie /mnt/files/Queue/
+      sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/Queue/" -r --format "{n} - {y} - 720p" --output "/mnt/files/Movies/" -non-strict
+    elif [ $height -ge 390 ]; then
+      echo 480p
+      mv $movie /mnt/files/Queue/
+      sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/Queue/" -r --format "{n} - {y} - 480p" --output "/mnt/files/Movies/" -non-strict
+    elif [ $height -ge 340 ]; then
+      echo 360p
+      mv $movie /mnt/files/Queue/
+      sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/Queue/" -r --format "{n} - {y} - 360p" --output "/mnt/files/Movies/" -non-strict
+    fi
+  done
+
+  echo Moving files from /mnt/files/Movies to /mnt/share/Movies...
+  ls /mnt/files/Movies
+  echo ...
+  mv /mnt/files/Movies/* /mnt/share/Movies/
+
+  find "/mnt/files/Queue/" -empty -type d -delete
+  find "/mnt/files/Complete/" -empty -type d -delete
+  find "/mnt/files/Movies/" -empty -type d -delete
+  echo Complete
+fi
+
+if [ -d "/mnt/files/Movies/" ]; then
+  echo Moving files from /mnt/files/Movies to /mnt/share/Movies...
+  ls /mnt/files/Movies
+  echo ...
+  mv /mnt/files/Movies/* /mnt/share/Movies/
+  find "/mnt/files/Queue/" -empty -type d -delete >/dev/null
+  find "/mnt/files/Complete/" -empty -type d -delete >/dev/null
+  find "/mnt/files/Movies/" -empty -type d -delete >/dev/null
+  echo Complete
+fi
+#echo Complete.
