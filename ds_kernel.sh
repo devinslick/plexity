349a127d1349aa2f462984a92ba52b4082af1895
Devin Slick <git@devinslick.com>
2016-01-24 17:33:09 -0600

    Update ds_kernel.sh

--- ds_kernel.sh
+++ ds_kernel.sh
@@ -5,14 +5,22 @@
 #
 wget "http://files.trendmicro.com/documentation/guides/deep_security/Kernel%20Support/9.6/Deep_Security_96_kernels_EN.html"
 RESULTS=$(sed -e '/centos7 (64-bit)/,/CloudLinux Kernels/!d' Deep_Security_96_kernels_EN.html | grep -F '.' | tail -1)
-rm Deep_Security_96_kernel_US*
+rm -f Deep_Security_96_kernels_EN.html*
 RESULTS=$(echo $RESULTS | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')
-echo Attempting to install CentOS 7 kernel $RESULTS
-install=$(yum install kernel-$RESULTS)
-if [[ $install == *"Nothing to do"* ]]
+
+installed=$(uname -a)
+if [[ $installed == *"$RESULTS"* ]]
 then
-  echo "There was nothing to do!"
+  echo "Fully updated!"
 else
-  echo "Yay, the kernel was updated.  Time to reboot"
-  reboot
+  echo Attempting to install CentOS 7 kernel $RESULTS
+  install=$(yum install kernel-$RESULTS)
+  if [[ $install == *"Nothing to do"* ]]
+  then
+    echo "There was nothing to do!"
+  else
+    echo "Yay, the kernel was updated.  Time to reboot"
+    reboot
+  fi
 fi
+
