cf9235dceb99815b1d4680f3986a202bf901ba64
Devin Slick <git@devinslick.com>
2016-01-31 18:02:11 -0600

    Update installPlex.sh

--- installPlex.sh
+++ installPlex.sh
@@ -1,12 +1,2 @@
 echo "If you have a PlexPass this script can automatically download and install the latest PlexPass Plex Server build."
-read -p "Do you have a PlexPass?? " answer
-    case ${answer:0:1} in y|Y )
-        echo "Please enter your Plex username or email address: "
-        read plexusername
-        echo "Please enter your Plex password: "
-        read plexpassword; break;;
-    ;;
-    * )
-        echo No
-    ;;
-esac
+read -n 1 -p "Do you have a PlexPass (y/n)? " answer
