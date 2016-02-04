2b76157767f6859e7d397135af73fe8a6a528ef8
Devin Slick <git@devinslick.com>
2016-02-03 18:01:01 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,18 +1,56 @@
 #!/bin/bash
+echo This script is intended to setup a Plex media server on CentOS 7.
+read -n 1 -p "Would you like to continue? [y/n]: " installContinue
+if [ $installContinue = 'n' ]; then
+  exit
+fi
 
 sudo bash
-su - root
 
-echo "Installing prerequisites and dependencies..."
-yum -y install make gcc pam-devel git wget open-vm-tools java
+grep -q ^flags.*\ hypervisor /proc/cpuinfo && echo "This machine is a virtual machine, installing VMware Tools..." && yum -y install open-vm-tools
 
-echo "Installing media libraries and video management applications..."
+echo "Installing server prerequisites and dependencies..."
+yum -y install yum make gcc pam-devel wget git
+
+echo "Installing various media plugins applications..."
 yum -y install http://li.nux.ro/download/nux/dextop/el7/x86_64/nux-dextop-release-0-5.el7.nux.noarch.rpm http://linuxdownload.adobe.com/linux/x86_64/adobe-release-x86_64-1.0-1.noarch.rpm
 yum -y install vlc smplayer ffmpeg HandBrake-{gui,cli} libdvdcss gstreamer{,1}-plugins-ugly gstreamer-plugins-bad-nonfree gstreamer1-plugins-bad-freeworld
 
-echo Installing transmission...
-yum -y install epel-* transmission transmission-daemon
-chkconfig transmission-daemon on
+read -n 1 -p "Would you like to install Transmission for torrenting? [y/n]: " installTransmission
+if [ $installTransmission = 'y' ]; then
+  echo Installing transmission...
+  yum -y install epel-* transmission transmission-daemon
+  chkconfig transmission-daemon on
+fi
+
+if [ $installTransmission = 'y' ]; then
+  echo "FileBot is a tool used to help automate renaming media."
+  echo "Since you chose to install Transmission, I'll go ahead and install FileBot for you as well.  You're welcome!"
+  Yum -y unzip java
+  wget http://downloads.sourceforge.net/project/filebot/filebot/FileBot_4.6.1/FileBot_4.6.1-portable.zip?r=http%3A%2F%2Fwww.filebot.net%2F&ts=1449410469&use_mirror=iweb
+  mkdir -p /scripts/filebot/
+  mv FileBot_4.6* /scripts/filebot/
+  unzip /scripts/filebot/FileBot_4.6* -d /scripts/filebot/ -x *.exe
+  rm -rf /root/scripts/filebot/*.zip
+  echo "FileBot has been installed!"
+fi
+
+echo "Trend Micro Deep Security..."
+echo "Please note: This will require you know the server address of your Deep Security Manager."
+read -n 1 -p "Would you like to install the Deep Security Agent? [y/n]: " installDeepSecurity
+if [ $installDeepSecurity = 'y' ]; then
+  echo Installing Deep Security Agent...
+  echo "Kernel updates should only be installed if they've been confirmed compatible with Deep Security."
+  echo "To avoid incompatibily, I'm excluding kernel updates from yum."
+  echo "Don't worry, I'll give you a script and a cronjob to automatically update to the latest supported kernel!"
+  echo exclude=kernel* redhat-release* >> /etc/yum.conf
+  wget https://Slick-DSM.devinslick.com:4119/software/agent/RedHat_EL7/x86_64/ -O /tmp/agent.rpm --no-check-certificate --quiet
+  rpm -ihv /tmp/agent.rpm
+  /opt/ds_agent/dsa_control -a dsm://Slick-DSM.devinslick.com:4120/
+  chkconfig iptables off
+  systemctl stop firewalld, systemctl disable firewalld
+  sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
+fi
 
 echo Importing scripts from github...
 mkdir /root/scripts/
@@ -21,15 +59,6 @@ git clone https://github.com/devinslick/plexity.git
 chmod +x /root/scripts/plexity/*.sh
 git clone https://github.com/devinslick/plexupdate.git
 
-echo Installing Deep Security Agent...
-echo exclude=kernel* redhat-release* >> /etc/yum.conf
-wget https://Slick-DSM.devinslick.com:4119/software/agent/RedHat_EL7/x86_64/ -O /tmp/agent.rpm --no-check-certificate --quiet
-rpm -ihv /tmp/agent.rpm
-/opt/ds_agent/dsa_control -a dsm://Slick-DSM.devinslick.com:4120/
-#no firewall, using ds firewall; Deep Security doesn’t support SELinux
-chkconfig iptables off
-systemctl stop firewalld, systemctl disable firewalld
-sed -i 's/enforcing/disabled/g' /etc/selinux/config /etc/selinux/config
 
 echo Installing and configuring automatic updates
 yum -y install yum-cron
@@ -40,10 +69,6 @@ sed -ie 's/random_sleep = 360/random_sleep = 0/' /etc/yum/yum-cron.conf
 sed -ie 's/assumeyes = True/assumeyes = False/' /etc/yum/yum-cron.conf
 systemctl restart yum-cron
 yum -y update
-
-echo Installing Filebot
-Wget http://downloads.sourceforge.net/project/filebot/filebot/FileBot_4.6.1/FileBot_4.6.1-portable.zip?r=http%3A%2F%2Fwww.filebot.net%2F&ts=1449410469&use_mirror=iweb
-Yum -y unzip
 
 echo Installing Plex...
 echo "If you have a PlexPass this script can automatically download and install the latest PlexPass Plex Server build."
