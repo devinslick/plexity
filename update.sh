4c74ec29d02bed710fba209ae18cc2d88b491e21
Devin Slick <git@devinslick.com>
2016-02-06 23:24:18 -0600

    Shhhh.   Cleanup output

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root/scripts/plexity
-git reset HEAD
-git checkout -- .
+git reset HEAD > /dev/null 2>&1
+git checkout -- . > /dev/null 2>&1
 git pull -f
-chmod +x *.sh
+chmod +x *.sh > /dev/null 2>&1
