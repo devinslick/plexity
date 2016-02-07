3d39f9577b79a47069788aa9caa13aac914780bd
Devin Slick <git@devinslick.com>
2016-02-06 23:32:13 -0600

    Cleanup output

--- ds_kernel.sh
+++ ds_kernel.sh
@@ -3,24 +3,22 @@
 #	AUTHOR: Devin Slick
 #	Script purpose: Install the latest CentOS 7 (x64) kernel supported with Trend Micro Deep Security 9.6
 #
-wget "http://files.trendmicro.com/documentation/guides/deep_security/Kernel%20Support/9.6/Deep_Security_96_kernels_EN.html"
-RESULTS=$(sed -e '/centos7 (64-bit)/,/CloudLinux Kernels/!d' Deep_Security_96_kernels_EN.html | grep -F '.' | tail -1)
-rm -f Deep_Security_96_kernels_EN.html*
+wget "http://files.trendmicro.com/documentation/guides/deep_security/Kernel%20Support/9.6/Deep_Security_96_kernels_EN.html" > /dev/null 2>&1
+RESULTS=$(sed -e '/centos7 (64-bit)/,/CloudLinux Kernels/!d' Deep_Security_96_kernels_EN.html | grep -F '.' | tail -1) > /dev/null 2>&1
+rm -f Deep_Security_96_kernels_EN.html* > /dev/null 2>&1
 RESULTS=$(echo $RESULTS | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')
-
 installed=$(uname -a)
 if [[ $installed == *"$RESULTS"* ]]
 then
-  echo "Fully updated!"
+  echo "You are already running the latest CentOS 7 kernel supported by Trend Micro Deep Security."
 else
   echo Attempting to install CentOS 7 kernel $RESULTS
   install=$(yum install kernel-$RESULTS)
   if [[ $install == *"Nothing to do"* ]]
   then
-    echo "There was nothing to do!"
+    echo "I've attempted to install a kernel but there was nothing to do."
   else
-    echo "Yay, the kernel was updated.  Time to reboot"
+    echo "Your kernel was updated - Time to Reboot!"
     reboot
   fi
 fi
-
