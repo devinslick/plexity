976811402182831f01521ef6545f1901a6d56446
Devin Slick <git@devinslick.com>
2016-03-12 16:28:27 -0600

    Automating decompression of compressed files....

--- import.sh
+++ import.sh
@@ -7,9 +7,18 @@ fi
 
 mkdir -p /mnt/files/Queue/TV/
 mkdir -p /mnt/files/Queue/Movies/
+mkdir -p /mnt/files/Queue/Compressed/
 mkdir -p /mnt/files/Trash/
 mkdir -p /mnt/files/Movies/
 mkdir -p /mnt/files/TV/
+
+echo "Looking for compresed files..."
+find /mnt/files/Complete/ -type f -iname "*.sfv" -exec mv {} /mnt/files/Queue/Compressed/ \; >/dev/null
+find /mnt/files/Complete/ -type f -iname "*.rar" -exec mv {} /mnt/files/Queue/Compressed/ \; >/dev/null
+find /mnt/files/Complete/ -type f -iname "*.r[0-9][0-9]" -exec mv {} /mnt/files/Queue/Compressed/ \; >/dev/null
+unrar x /mnt/files/Queue/Compressed/*.rar
+mv /mnt/files/Queue/Compressed/* /mnt/files/Complete/
+
 
 find /mnt/files/Complete/ -type f -size -40M -exec mv {} /mnt/files/Trash/ \; >/dev/null
 find /mnt/files/Complete/ -type f -size -200M -iname "*sample*" -exec mv {} /mnt/files/Trash/ \; >/dev/null
@@ -38,12 +47,13 @@ echo ...
 sudo /root/scripts/filebot/filebot.sh -rename "/mnt/files/TV/" -r --format "{n}/Season {s}/{n} - S{s.pad(2)}E{e.pad(2)} - {t}" --output "/mnt/share/TV Shows/" -non-strict -no-xattr
 find "/mnt/files/TV/" -empty -type d -delete
 
-echo "Checking /mnt/files/Trash for compressed files..."
-if [ -f *.zip ] ||  [ -f *.rar ] ||  [ -f *.001 ]; then
-  echo "Compressed files were found.  Please move or delete these so automatic trash cleanup can continue."
-else
-  echo "Cleaning /mnt/files/Trash..."
-  /root/scripts/filebot/filebot.sh -script fn:cleaner /mnt/files/Trash --def root=y
-fi
+#   Removing this section while I work on automatically decompressing archives.
+#echo "Checking /mnt/files/Trash for compressed files..."
+#if [ -f /mnt/files/Trash/*.zip ] ||  [ -f /mnt/files/Trash/*.rar ] ||  [ -f *.001 ]; then
+#  echo "Compressed files were found.  Please move or delete these so automatic trash cleanup can continue."
+#else
+#  echo "Cleaning /mnt/files/Trash..."
+#  /root/scripts/filebot/filebot.sh -script fn:cleaner /mnt/files/Trash --def root=y
+#fi
 
 echo Complete
