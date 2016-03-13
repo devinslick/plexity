a52d1d1a5111ea9384b47afaefb3a77ceb81532f
Devin Slick <git@devinslick.com>
2016-03-12 21:20:47 -0600

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,31 +1,42 @@
 #!/bin/bash
 #
-# This script is intended to be used to manage cronjobs on CentOS 7 devices without the plex server installed.
+# This script is intended to be used to manage cronjobs on CentOS 7 devices
 # 
 
-#update scripts from repo at 2:30am
-echo "30 2 * * * /root/scripts/plexity/update.sh" | crontab -
-#rebuild cronjobs at 3am
-(crontab -l ; echo "0 3 * * * /root/scripts/plexity/cron.sh > /dev/null 2>&1") | crontab -
+#check for the Trend Micro Deep Security Agent
 dsagent=$(yum info ds_agent.x86_64 | grep "Repo        : installed")
+
 if [[ $dsagent == *"installed"* ]]
 then
-  #update installed packages other than kernel at 3:30am
-  (crontab -l ; echo "30 3 * * * /usr/bin/yum -y -e 0 -x kernel* update") | crontab -
-  #update the kernel to the latest supported by Deep Security at 4am
-  (crontab -l ; echo "0 4 * * * /root/scripts/plexity/ds_kernel.sh") | crontab -
   #send a heartbeat to the Deep Security Manager every 30 minutes.
-  (crontab -l ; echo "*/30 * * * * /opt/ds_agent/dsa_control -m > /dev/null 2>&1") | crontab -
-else
-  #update installed packages at 4am
-  (crontab -l ; echo "0 4 * * * /usr/bin/yum -y -e 0 update") | crontab -
+  echo "*/30 * * * * /opt/ds_agent/dsa_control -m >> /dev/null 2>&1" | crontab -
 fi
+
+#update scripts at 2:30am
+(crontab -l ; echo "30 2 * * * /root/scripts/plexity/update.sh >> /mnt/files/log 2>&1") | crontab -
+
+#rebuild cronjobs at 2:40am
+(crontab -l ; echo "40 2 * * * /root/scripts/plexity/cron.sh > /dev/null 2>&1") | crontab -
+
+if [[ $dsagent == *"installed"* ]]
+then
+  #update packages other than kernel at 3:30am
+  (crontab -l ; echo "45 2 * * * /usr/bin/yum -y -e 0 -x kernel* update >> /mnt/files/log 2>&1") | crontab -
+else
+  (crontab -l ; echo "45 2 * * * /usr/bin/yum -y -e 0 update >> /mnt/files/log 2>&1") | crontab -
+fi
+
+if [[ $dsagent == *"installed"* ]]
+then
+  #update the kernel to the latest supported by Deep Security at 4am
+  (crontab -l ; echo "0 3 * * * /root/scripts/plexity/ds_kernel.sh >> /mnt/files/log 2>&1") | crontab -
+fi
+
+#check for Plex Media Server
 plexmediaserver=$(yum info plexmediaserver.x86_64 | grep "Repo        : installed")
+
 if [[ $plexmediaserver == *"installed"* ]]
 then
   #update plexmediaserver at 4:30am
-  (crontab -l ; echo "30 4 * * * /root/scripts/plexupdate/plexupdate.sh") | crontab -
+  (crontab -l ; echo "30 3 * * * /root/scripts/plexupdate/plexupdate.sh >> /mnt/files/log 2>&1") | crontab -
 fi
-
-#filebot update not working from cron
-#(crontab -l ; echo "0 3 * * * /root/scripts/filebot/update-filebot.sh") | crontab -
