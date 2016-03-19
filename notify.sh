a09b57db47075f0f94b5be550bbbfaca538b2620
Devin Slick <github@devinslick.com>
2016-03-19 18:30:09 -0500

    Improved subject/title of notifications

--- notify.sh
+++ notify.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 URL="https://api.pushover.net/1/messages.json"
-TITLE="Plexity Alert"
+PUSHTITLE="Plexity - "$HOSTNAME
+EMAILSUBJECT="plexity@"$HOSTNAME
 PRIORITY=1
 MESSAGE=$*
 if [ ${#MESSAGE} == 0 ]
@@ -13,13 +14,13 @@ then # if api key has been defined
    then  # read those files to variable, take action
       APP_KEY=$(cat /etc/pushover.api.key)  
       USER_KEY=$(cat /etc/pushover.user.key)
-      RESPONSE=$(curl -s --data token=$APP_KEY --data user=$USER_KEY --data-urlencode title="$TITLE" --data priority=$PRIORITY --data-urlencode message="$MESSAGE" $URL)
+      RESPONSE=$(curl -s --data token=$APP_KEY --data user=$USER_KEY --data-urlencode title="$PUSHTITLE" --data priority=$PRIORITY --data-urlencode message="$MESSAGE" $URL)
    else # if one has been defined but not the other
       echo "Error: Pushover API key was defined at /etc/pushover.api.key but user key (/etc/pushover.user.key) was missing"
       if [ -f /etc/emailaddress ]
       then
          logger "Plexity was unable to use Pushover API, falling back to email notification"
-         echo $MESSAGE | /usr/bin/mailx -E -r plexity@$(cat /etc/hostname) -s $(cat /etc/hostname) $(cat /etc/emailaddress)
+         echo $MESSAGE | /usr/bin/mailx -E -r "plexity"@$(cat /etc/hostname) -s $EMAILSUBJECT $(cat /etc/emailaddress)
       else
          logger "Plexity was unable to use Pushover or send email.  Please read Plexity documentation."
       fi
@@ -27,7 +28,7 @@ then # if api key has been defined
 else # failed the first test, no pushover api key defined.
    if [ -f /etc/emailaddress ]
       then
-         echo $MESSAGE | /usr/bin/mailx -E -r plexity@$(cat /etc/hostname) -s $(cat /etc/hostname) $(cat /etc/emailaddress)
+         echo $MESSAGE | /usr/bin/mailx -E -r "plexity"@$(cat /etc/hostname) -s $EMAILSUBJECT $(cat /etc/emailaddress)
       else
          logger "Plexity was unable to use Pushover or send email.  Please read Plexity documentation."
    fi
