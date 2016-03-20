7da145afa87dd690cbaf2842d0f36e234cca41a4
Devin Slick <github@devinslick.com>
2016-03-20 15:36:42 -0500

    Added crontab

---
+++ update-crontab.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#
+# This script is intended to be used to manage cronjobs on CentOS 7 devices
+# 
+echo "Rebuilding Plexity cron jobs for "$HOSTNAME"..."
+echo -en "Checking for the Trend Micro Deep Security Agent:"
+dsagent=$(yum info ds_agent.x86_64 | grep "Repo        : installed")
+echo -en "\nChecking for the Plex Media Server:"
+plexmediaserver=$(yum info plexmediaserver.x86_64 | grep "Repo        : installed")
+
+if [[ $dsagent == *"installed"* ]]
+then
+  #send a heartbeat to the Deep Security Manager every 30 minutes.
+  echo "*/30 * * * * /opt/ds_agent/dsa_control -m >> /dev/null 2>&1" | crontab -
+fi
+
+#update scripts at 2:30am
+(crontab -l ; echo "30 2 * * * /root/scripts/plexity/update-scripts.sh | /root/scripts/plexity/notify.sh") | crontab -
+
+#rebuild cronjobs at 2:40am
+(crontab -l ; echo "40 2 * * * /root/scripts/plexity/update-crontab.sh | /root/scripts/plexity/notify.sh") | crontab -
+
+if [[ $dsagent == *"installed"* ]]
+then
+  #update packages other than kernel at 3:30am
+  (crontab -l ; echo "45 2 * * * /root/scripts/plexity/update-centos.sh | /root/scripts/plexity/notify.sh") | crontab -
+else
+  (crontab -l ; echo "45 2 * * * /root/scripts/plexity/update-centos.sh | /root/scripts/plexity/notify.sh") | crontab -
+fi
+
+if [[ $dsagent == *"installed"* ]]
+then
+  #update the kernel to the latest supported by Deep Security at 4am
+  (crontab -l ; echo "0 3 * * * /root/scripts/plexity/ds_kernel.sh | /root/scripts/plexity/notify.sh") | crontab -
+fi
+
+if [[ $plexmediaserver == *"installed"* ]]
+then
+  #update plexmediaserver at 4:30am
+  (crontab -l ; echo "30 3 * * * /root/scripts/plexupdate/plexupdate.sh | /root/scripts/plexity/notify.sh") | crontab -
+fi
+echo -e "\nCrontab has been updated:"
+crontab -l
