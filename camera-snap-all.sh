50e78a7fc9632a020d0d87416ecb43a8f16474cf
Devin Slick <git@devinslick.com>
2016-03-25 17:32:46 -0500

    Move variables from /etc/ to /var/plexity/

--- camera-snap-all.sh
+++ camera-snap-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ls /etc/*.camera | while read cameraURL; do
+ls /var/plexity/*.camera | while read cameraURL; do
    echo "Processing file '$cameraURL'"
    mkdir -p /mnt/files/Security/$(basename $(ls $cameraURL | cut -d '.' -f1))/$(date '+%Y%m%d') 
    /bin/wget -O /mnt/files/Security/$(basename $(ls $cameraURL | cut -d '.' -f1))/$(date +"%Y%m%d")/$(date +"%Y-%m-%d-%H:%M:%S").jpg -x $(cat $cameraURL)
