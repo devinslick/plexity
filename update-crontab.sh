7150e4d6c12ed2c6b7fe5272483267342313d184
Devin Slick <git@devinslick.com>
2016-03-26 00:15:18 -0500

    Move crontab to plexity user, don't use root
    Previously used the root crontab, now all jobs are scheduled under plexity

--- update-crontab.sh
+++ update-crontab.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # This script is intended to be used to manage cronjobs on CentOS 7 devices
-# 
+#
 echo "Rebuilding Plexity cron jobs for "$HOSTNAME"..."
 echo -en "Checking for the Trend Micro Deep Security Agent:"
 dsagent=$(yum info ds_agent.x86_64 | grep "Repo        : installed")
@@ -11,33 +11,33 @@ plexmediaserver=$(yum info plexmediaserver.x86_64 | grep "Repo        : installe
 if [[ $dsagent == *"installed"* ]]
 then
   #send a heartbeat to the Deep Security Manager every 30 minutes.
-  echo "*/30 * * * * /opt/ds_agent/dsa_control -m >> /dev/null 2>&1" | crontab -
+  echo "*/30 * * * * /opt/ds_agent/dsa_control -m >> /dev/null 2>&1" | crontab -u plexity -
 fi
 
 #update scripts at 2:30am
-(crontab -l ; echo "30 2 * * * /opt/plexity/update-scripts.sh | /opt/plexity/notify.sh") | crontab -
+(crontab -u plexity -l ; echo "30 2 * * * /opt/plexity/update-scripts.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 
 #rebuild cronjobs at 2:40am
-(crontab -l ; echo "40 2 * * * /opt/plexity/update-crontab.sh | /opt/plexity/notify.sh") | crontab -
+(crontab -u plexity -l ; echo "40 2 * * * /opt/plexity/update-crontab.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 
 if [[ $dsagent == *"installed"* ]]
 then
   #update packages other than kernel at 3:30am
-  (crontab -l ; echo "45 2 * * * /opt/plexity/update-centos.sh | /opt/plexity/notify.sh") | crontab -
+  (crontab -u plexity -l ; echo "45 2 * * * /opt/plexity/update-centos.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 else
-  (crontab -l ; echo "45 2 * * * /opt/plexity/update-centos.sh | /opt/plexity/notify.sh") | crontab -
+  (crontab -u plexity -l ; echo "45 2 * * * /opt/plexity/update-centos.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 fi
 
 if [[ $dsagent == *"installed"* ]]
 then
   #update the kernel to the latest supported by Deep Security at 4am
-  (crontab -l ; echo "0 3 * * * /opt/plexity/ds_kernel.sh | /opt/plexity/notify.sh") | crontab -
+  (crontab -u plexity -l ; echo "0 3 * * * /opt/plexity/ds_kernel.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 fi
 
 if [[ $plexmediaserver == *"installed"* ]]
 then
   #update plexmediaserver at 4:30am
-  (crontab -l ; echo "30 3 * * * /opt/plexupdate/plexupdate.sh | /opt/plexity/notify.sh") | crontab -
+  (crontab -u plexity -l ; echo "30 3 * * * /opt/plexupdate/plexupdate.sh | /opt/plexity/notify.sh") | crontab -u plexity -
 fi
 echo -e "\nCrontab has been updated:"
-crontab -l
+crontab -u plexity -l
