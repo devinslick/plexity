9c063d8e37fcb6969288fa9554b837629e4b9e42
Devin Slick <git@devinslick.com>
2016-04-01 19:38:33 -0500

    Removed .x86_64 from kernel for install command

--- ds_kernel.sh
+++ ds_kernel.sh
@@ -10,6 +10,7 @@ wget -q "http://files.trendmicro.com/documentation/guides/deep_security/Kernel%2
 RESULTS=$(sed -e '/centos7 (64-bit)/,/CloudLinux Kernels/!d' Deep_Security_96_kernels_EN.html | grep -F '.' | tail -1) > /dev/null 2>&1
 rm -f Deep_Security_96_kernels_EN.html* > /dev/null 2>&1
 RESULTS=$(echo $RESULTS | sed -e :a -e 's/<[^>]*>//g;/</N;//ba')
+RESULTS=$(sed 's/.x86_64/ /g' <<<"$RESULTS")
 if [[ $installed == *"$RESULTS"* ]]
 then
   echo "You are already running the latest supported CentOS 7 kernel."
