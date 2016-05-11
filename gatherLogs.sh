7333d0dbf6dca99a25550eb9d6f48da52fbdebe6
Devin Slick <git@devinslick.com>
2016-05-10 19:28:36 -0500

    Send logs with notify.sh and archive

--- gatherLogs.sh
+++ gatherLogs.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 today=$(date +'%b %d')
-cat /var/log/yum.log | grep "$today" >> /var/log/plexity/today.lo
-
+cat /var/log/yum.log | grep "$today" >> /var/log/plexity/today.log
+cat /var/log/plexity/today.log | /opt/plexity/notify.sh
+mv /var/log/plexity/today.log /var/log/plexity/$(date +'%Y%m%d').log
