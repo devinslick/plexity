a0e31825ff8552d44509e3c3e147d24a94c0811c
Devin Slick <git@devinslick.com>
2016-05-10 19:13:26 -0500

    Removed extra space

--- update-scripts.sh
+++ update-scripts.sh
@@ -7,8 +7,8 @@ OUTPUT="$(git pull -f)"
 
 if [[ $OUTPUT == *"Already up-to-date"* ]]
 then
-   echo $(date +'%b %d %H:%m:%M') " Plexity script update is already up to date" | tee -a /var/log/plexity/today.log
+   echo $(date +'%b %d %H:%m:%M')" Plexity script update is already up to date" | tee -a /var/log/plexity/today.log
 else
-   echo $(date +'%b %d %H:%m:%M') " Plexity script update complete!" | tee -a /var/log/plexity/today.log
+   echo $(date +'%b %d %H:%m:%M')" Plexity script update complete!" | tee -a /var/log/plexity/today.log
 fi
 
