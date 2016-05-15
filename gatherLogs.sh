df26a7403d73d7ecb100094cf4618b4164a65d33
Devin Slick <git@devinslick.com>
2016-05-14 22:23:00 -0500

    Update gatherLogs.sh

--- gatherLogs.sh
+++ gatherLogs.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-today=$(date +'%b %d')
-cat /var/log/yum.log | grep "$today" >> /var/log/plexity/today.log
 cat /var/log/plexity/today.log | /opt/plexity/notify.sh
-mv /var/log/plexity/today.log /var/log/plexity/$(date +'%Y%m%d').log
