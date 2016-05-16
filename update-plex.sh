54b7192c8db44ef5eb85c269b6f110f62cdd660d
Devin Slick <git@devinslick.com>
2016-05-15 19:28:28 -0500

    need sudo to check yum.log

--- update-plex.sh
+++ update-plex.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo /opt/plexity-plexupdate/plexupdate.sh
-grep plexmediaserver /var/log/yum.log | grep "$(date +'%b %d %H')" >> /var/log/plexity/$(date '+%Y%m%d').log
+sudo grep plexmediaserver /var/log/yum.log | grep "$(date +'%b %d %H')" >> /var/log/plexity/$(date '+%Y%m%d').log
