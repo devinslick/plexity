9ce693b13179c0fb0fe4b31d2bb885aede058fc8
Devin Slick <git@devinslick.com>
2016-06-04 16:21:59 -0500

    Added function to install DSM

--- setup.sh
+++ setup.sh
@@ -1,5 +1,44 @@
 #!/bin/bash
 echo This script is intended to setup a Plex media server on CentOS 7.
+
+
+function installdsm
+{
+  read -n 1 -p "Would you like to install the Deep Security Manager? [y/n]: " installDSM
+  if [ $installDSM = 'y' ]; then
+	  if [ -f /var/plexity/dsm.key ]; then
+		  $license=(cat /var/plexity/dsm.license)
+	  else
+		  read -n 1 -p "Would you like to add your license key now? [y/n]: " installDSM
+		  if [ $installDSM = 'y' ]; then
+        echo -n "Enter your Deep Security Manager license key: "
+			  read -u license
+			  echo $license > /var/plexity/dsm.license
+		  else
+			  license=""
+		  fi
+    fi
+  	wget http://files.trendmicro.com/products/deepsecurity/en/9.6/9.6_ServicePack1/Manager-Linux-9.6.4000.x64.sh -O /tmp/dsmsetup.sh --quiet 
+  	echo "AddressAndPortsScreen.NewNode=True" > /tmp/install.config
+  	echo "UpgradeVerification.Overwrite=False" >> /tmp/install.config
+  	echo "LicenseScreen.License.-1=$license" >> /tmp/install.config
+  	echo "DatabaseScreen.DatabaseType=Embedded" >> /tmp/install.config
+  	echo "CredentialsScreen.Administrator.Username=masteradmin" >> /tmp/install.config
+  	echo "CredentialsScreen.Administrator.Password=masteradmin" >> /tmp/install.config
+  	echo "CredentialsScreen.UseStrongPasswords=False" >> /tmp/install.config
+  	echo "SecurityUpdateScreen.UpdateComponents=True" >> /tmp/install.config
+  	echo "SoftwareUpdateScreen.UpdateSoftware=True" >> /tmp/install.config
+  	echo "RelayScreen.Install=False" >> /tmp/install.config
+  	echo "SmartProtectionNetworkScreen.EnableFeedback=False" >> /tmp/install.config
+  	chmod +x /tmp/dsmsetup.sh
+  	/tmp/dsmsetup.sh -q -console -varfile /tmp/install.config
+  	chkconfig dsm_s on
+  	systemctl start dsm_s
+	  echo "https://"$(hostname)":4119/SignIn.screen"
+	  echo "Login using the default username and password: masteradmin:masteradmin" 
+  fi
+}
+
 read -n 1 -p "Would you like to continue? [y/n]: " installContinue
 if [ $installContinue = 'n' ]; then
   exit
@@ -211,3 +250,5 @@ yum -y update
 if [ $installDeepSecurity = 'y' ]; then
   /opt/plexity/ds_kernel.sh
 fi
+
+
