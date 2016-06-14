263078357c991e4e7d203fc8d7740c90071092f1
Devin Slick <devinslick@users.noreply.github.com>
2016-06-14 08:02:07 -0500

    Create getManaged.py

---
+++ scripts/deepsecuritymanager/getManaged.py
@@ -0,0 +1,8 @@
+import sdk
+mgr = sdk.dsm.Manager(hostname="localhost", username="datareader", password="abc123abc", ignore_ssl_validation=True)
+mgr.sign_in()
+mgr.computers.get()
+for computer_id in mgr.computers.find(overall_status='Managed.*'):
+  computer = mgr.computers[computer_id]
+  print "\t{}\t{}".format(computer.name, computer.overall_status)
+mgr.sign_out()
