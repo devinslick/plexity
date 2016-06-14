85014d646b4e8f8ec84c4df3f47313f03f23f4e5
Devin Slick <devinslick@users.noreply.github.com>
2016-06-14 14:54:32 -0500

    Update notifyUnmanaged.sh

--- scripts/deepsecuritymanager/notifyUnmanaged.sh
+++ scripts/deepsecuritymanager/notifyUnmanaged.sh
@@ -2,6 +2,7 @@ numberUnmanaged=$(/opt/plexity/scripts/deepsecuritymanager/countUnmanaged.sh)
 if [ $numberUnmanaged -gt "0" ];
 then
   #There are unmanaged agents in the Deep Security Manager"
+  echo $numberUnmanaged
 else
   #All are managed
   /opt/plexity/scripts/deepsecuritymanager/getUnmanaged.sh
