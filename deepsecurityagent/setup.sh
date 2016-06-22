d8e6e3732cca56ab348a09bcd1ed4593fa5ffd23
Devin Slick <devinslick@users.noreply.github.com>
2016-06-21 19:51:34 -0500

    Update setup.sh

--- deepsecurityagent/setup.sh
+++ deepsecurityagent/setup.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+#
+#	AUTHOR: Devin Slick
+#	Script purpose: Install/update the latest Trend Micro Deep Security 9.6 agent for CentOS 7
+#
 wget http://files.trendmicro.com/products/deepsecurity/en/9.6/Agent-RedHat_EL7-9.6.2-5027.x86_64.zip -O /tmp/dsa.zip
 unzip -n /tmp/dsa.zip -x *.dsp -d /tmp/dsa/
 if [ ! -d "/opt/ds_agent" ];
