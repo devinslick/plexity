0ccbb962ca458d4be7881a8db3ffdff8c15a845b
Devin Slick <github@devinslick.com>
2016-06-21 19:17:14 -0500

    Check input, getCertificate

--- scripts/letsencrypt/getCertificate.sh
+++ scripts/letsencrypt/getCertificate.sh
@@ -1,1 +1,14 @@
-/opt/letsencrypt/letsencrypt-auto --agree-tos --keep --rsa-key-size 4096 --standalone certonly -m $(cat /var/plexity/ssl.emailaddress) -d $(hostname)
+email=$(cat /var/plexity/ssl.emailaddress)
+if [ ${#email} -lt 6 ]
+then
+  echo "A valid email address was not found in /var/plexity/ssl.emailaddress"
+  exit 
+fi
+
+name=$(cat /var/plexity/ssl.hostname)
+if [ ${#name} -lt 6 ]
+then
+  echo "A valid FQDN was not found in /var/plexity/ssl.emailaddress"
+  exit 
+fi
+/opt/letsencrypt/letsencrypt-auto --agree-tos --keep --rsa-key-size 4096 --standalone certonly -m $email -d $name --test-cert
