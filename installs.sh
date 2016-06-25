5c1b81cf995e209cb157055232a24feacc674452
Devin Slick <github@devinslick.com>
2016-06-25 18:19:16 -0500

    update

---
+++ installs.sh
@@ -0,0 +1,8 @@
+while read app; do
+  if [[ ${app:0:1} == '*' ]];
+  then
+    /opt/plexity/$(echo $app| cut -d "*" -f 1)/setup.sh
+  else
+    sudo yum install $app
+  fi
+done < /var/plexity/desired.apps
---
+++ updates.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+plexmediaserver=$(rpm -qa | grep plexmediaserver)
+deepsecurityagent=$(rpm -qa | grep ds_agent)
+
+if [ ${#plexmediaserver} -gt 0 ];
+then
+  /opt/plexity/plexmediaserver/update.sh
+fi
+
+if [ ${#deepsecurityagent} -gt 0 ];
+then
+  yum -y -e 0 update -x 'kernel*'
+  /opt/plexity/deepsecurityagent/updatekernel.sh
+else
+  yum -y -e 0 update
+fi
