a9115fd00e43d978c2e6836368c6e6f0636f6abe
Devin Slick <devinslick@users.noreply.github.com>
2016-06-25 19:25:26 -0500

    Added newlines

--- pickApps.sh
+++ pickApps.sh
@@ -2,40 +2,50 @@ read -n 1 -p "Plex Media Server? [y/n]: " plexmediaserver
 if [ $plexmediaserver = 'y' ];
 then
   echo '*plexmediaserver' >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/*plexmediaserver/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
 read -n 1 -p "Transmission Server? [y/n]: " transmission
 if [ $transmission = 'y' ];
 then
   echo transmission >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/transmission/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
 read -n 1 -p "Deep Security Manager? [y/n]: " deepsecuritymanager
 if [ $deepsecuritymanager = 'y' ];
 then
   echo '*deepsecuritymanager' >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/*deepsecuritymanager/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
 read -n 1 -p "Deep Security Agent? [y/n]: " deepsecuritymanager
 if [ $deepsecurityagent = 'y' ];
 then
   echo '*deepsecurityagent' >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/*deepsecurityagent/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
 read -n 1 -p "HandBrake video editor? [y/n]: " handbrake
 if [ $handbrake = 'y' ];
 then
   echo 'HandBrake-{gui,cli}' >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/HandBrake-{gui,cli}/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
 read -n 1 -p "SMPlayer, VLC, and codecs for media playback? [y/n]: " players
@@ -44,9 +54,11 @@ then
   echo 'vlc' >> /var/plexity/desired.apps
   echo 'smplayer' >> /var/plexity/desired.apps
   echo 'libdvdcss gstreamer{,1}-plugins-ugly gstreamer-plugins-bad-nonfree gstreamer1-plugins-bad-freeworld' >> /var/plexity/desired.apps
+  echo "\n"
 else
   sed -n -i '/vlc/!p' /var/plexity/desired.apps
   sed -n -i '/smplayer/!p' /var/plexity/desired.apps
   sed -n -i '/libdvdcss gstreamer{,1}-plugins-ugly gstreamer-plugins-bad-nonfree gstreamer1-plugins-bad-freeworld/!p' /var/plexity/desired.apps
+  echo "\n"
 fi
 
