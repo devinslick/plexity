ee5daa9887b5fefd1cf9118e767774859dee0a14
Devin Slick <devinslick@users.noreply.github.com>
2016-08-28 10:06:38 -0500

    Update yesterday.sh

--- logs/yesterday.sh
+++ logs/yesterday.sh
@@ -1,15 +1,17 @@
-#!/bin/bash
-wget $(cat /var/plexity/homelog) -O /tmp/home-temp.log
+/opt/gdrive export $(/opt/gdrive list | grep log | head -1 | cut -d ' ' -f 1)
 #remove double quotes
-sed -i 's/\"//g' /tmp/home-temp.log
+cp log.csv log-$(date --date="yesterday" +"%Y%m%d").csv
+/opt/gdrive upload log-$(date --date="yesterday" +"%Y%m%d").csv -p $(/opt/gdrive list | grep HistoricalLogs | cut -d ' ' -f 1)
+sed -i 's/\"//g' log.csv
 #replace dates with padded days
-sed -i "s/$(date --date="yesterday" +"%B %d, %Y")/$(date --date="yesterday" +"%Y%m%d")/g" /tmp/home-temp.log
+sed -i "s/$(date --date="yesterday" +"%B %d, %Y")/$(date --date="yesterday" +"%Y%m%d")/g" log.csv
 #replace dates without padded days
-sed -i "s/$(date --date="yesterday" +"%B %-d, %Y")/$(date --date="yesterday" +"%Y%m%d")/g" /tmp/home-temp.log
-sed -i "s/$(date --date="yesterday" +"%Y%m%d") at /$(date --date="yesterday" +"%Y%m%d"),/g" /tmp/home-temp.log
+sed -i "s/$(date --date="yesterday" +"%B %-d, %Y")/$(date --date="yesterday" +"%Y%m%d")/g" log.csv
+sed -i "s/$(date --date="yesterday" +"%Y%m%d") at /$(date --date="yesterday" +"%Y%m%d"),/g" log.csv
 #Only keep lines containing today's date
-cat /tmp/home-temp.log | grep $(date --date="yesterday" +"%Y%m%d") > /tmp/home-$(date --date="yesterday" +"%Y%m%d").log
-rm -rf /tmp/home-temp.log
-cat /tmp/home-$(date --date="yesterday" +"%Y%m%d").log
+cat log.csv | grep $(date --date="yesterday" +"%Y%m%d") > /tmp/$(date --date="yesterday" +"%Y%m%d").log
+#cat /tmp/$(date --date="yesterday" +"%Y%m%d").log
 mkdir -p /media/share/Logs
-mv -n /tmp/home-$(date --date="yesterday" +"%Y%m%d").log /media/share/Logs/
+cp -n /tmp/$(date --date="yesterday" +"%Y%m%d").log /media/share/Logs/
+rm -rf log.csv
+/opt/gdrive delete $(/opt/gdrive list | grep log | cut -d ' ' -f 1)
