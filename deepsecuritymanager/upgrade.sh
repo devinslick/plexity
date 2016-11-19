bc683a9dacaceae25935483cbf569343fb407d44
Devin Slick <devinslick@users.noreply.github.com>
2016-11-19 12:57:29 -0600

    Create upgrade.sh

---
+++ deepsecuritymanager/upgrade.sh
@@ -0,0 +1,3 @@
+wget http://files.trendmicro.com/products/deepsecurity/9.6_SP1_P1/DeepSecurityManager/Manager-Linux-9.6.4064.x64.sh -O /tmp/dsmsetup.sh --quiet 
+chmod +x /tmp/dsmsetup.sh
+/tmp/dsmsetup.sh -q 
