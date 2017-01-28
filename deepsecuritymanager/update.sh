12e2595f9cf61a060dd21d29b47e701d66226828
Devin Slick <devinslick@users.noreply.github.com>
2017-01-28 17:53:52 -0600

    Update update.sh

--- deepsecuritymanager/update.sh
+++ deepsecuritymanager/update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo This script will update Deep Security Manager
-wget http://files.trendmicro.com/products/deepsecurity/en/9.6/9.6_ServicePack1/Manager-Linux-9.6.4014.x64.sh -O /tmp/dsmupdate.sh --quiet
+wget http://files.trendmicro.com/products/deepsecurity/en/9.6/Manager-Linux-9.6.4085.x64.sh -O /tmp/dsmupdate.sh
+chmod +x /tmp/dsmupdate.sh
 /tmp/dsmupdate.sh -q
-
